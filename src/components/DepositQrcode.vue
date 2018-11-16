748df9747dbcfee27cb97a17d4616f818390ee4d
Nitro888 <nitro88x@gmail.com>
2018-11-16 20:35:40 +0900

    DepositQrcode

--- src/components/DepositQrcode.vue
+++ src/components/DepositQrcode.vue
@@ -1,12 +1,34 @@
 <template>
-  <div class="main">
-    <h1>This is an deposit page</h1>
+  <div>
+    <vue-bstcode
+      :bgSrc='bgSrc'
+      :logoSrc="logoSrc"
+      :text="address"
+      :height="size"
+      :width="size"
+    ></vue-bstcode>
+    <a :href="url" target="_blank">{{address}}</a>
   </div>
 </template>
+
 <script>
-
-
+  import VueBstcode from 'vue-bstcode'
+  export default {
+    data () {
+      return {
+        bgSrc: null,    // todo : load something
+        logoSrc: null,  // todo : load leeyeon logo
+        address: window.wallet.account.address(),
+        url: window.wallet.utils.linkAddress(window.wallet.account.address()),
+        size: 256
+      }
+    },
+    components: {
+      VueBstcode
+    }
+  }
 </script>
+
 <style scoped>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,19 +3,19 @@ import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import store from './store'
-import 'leeyeon-wallet-core';
+import 'leeyeon-wallet-core'
 
 Vue.use(BootstrapVue)
 
 window.wallet.start({
-  "name"   : "ropsten",
-  "type"   : "wss",
-  "network": {"http"    : "https://ropsten.infura.io",
-              "wss"    : "wss://ropsten.infura.io/ws",
-              "api"    : "https://api-ropsten.etherscan.io",
-              "href"    : "https://ropsten.etherscan.io"},
-  "erc20s" : {"0xdF60Ce3dE7b799Ba695A3F049E03c94f8b70fD6A":["xnt",""]},
-  "erc721s": {}
+  'name': 'ropsten',
+  'type': 'wss',
+  'network': { 'http': 'https://ropsten.infura.io',
+    'wss': 'wss://ropsten.infura.io/ws',
+    'api': 'https://api-ropsten.etherscan.io',
+    'href': 'https://ropsten.etherscan.io' },
+  'erc20s': {},
+  'erc721s': {}
 })
 
 Vue.config.productionTip = false
@@ -23,5 +23,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
