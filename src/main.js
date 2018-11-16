18873052b2a75234f51fa4f5f3a9697c6f522412
elegant651 <korea651@gmail.com>
2018-11-16 12:37:02 +0900

    binding to data on transaction history

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ import 'leeyeon-wallet-core';
 
 Vue.use(BootstrapVue)
 
+window.wallet.start({
+  "name"   : "ropsten",
+  "type"   : "wss",
+  "network": {"http"    : "https://ropsten.infura.io",
+              "wss"    : "wss://ropsten.infura.io/ws",
+              "api"    : "https://api-ropsten.etherscan.io",
+              "href"    : "https://ropsten.etherscan.io"},
+  "erc20s" : {"0xdF60Ce3dE7b799Ba695A3F049E03c94f8b70fD6A":["xnt",""]},
+  "erc721s": {}
+})
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/TransactionHistory.vue
+++ src/views/TransactionHistory.vue
@@ -2,13 +2,13 @@
   <div class="menu">
     <h1>Transaction List</h1>
     <b-list-group class="blg">
-      <b-list-group-item class="menu d-flex justify-content-between align-items-center" v-for="item in transactions" :key="item.hash">
+      <b-list-group-item class="menu d-flex justify-content-between align-items-center" v-for="item in transactions" :key="item.index">
           <img :src="getTokenImg" />
           <div class="detail">
-            <p class="hash">{{item.hash}}</p>
-            <p class="name">{{item.name}} : {{item.balance}}</p>
+            <p class="nonce">#{{item.nonce}} <span class="label">Confirmed</span></p>
+            <p class="name">{{item.value}} ETH</p>
             <p class="desc">{{item.from}} => {{item.to}}</p>
-            <p class="date">{{item.date}}</p>
+            <p class="date">{{item.timestamp}}</p>            
           </div>             
       </b-list-group-item>      
     </b-list-group>
@@ -16,28 +16,21 @@
 </template>
 
 <script>
-
+/* eslint-disable no-console */
 export default {  
   name: 'transactionlist',
 
   data() {
     return {
       transactions: [
-        {
-          hash: '0dfdsdf',
-          name: 'ETH',
-          from: '0x0123f',
-          to: '0x04343',          
-          balance: 10,
-          date: '2018-11-14 18:23'
-        },
-        {
-          hash: '0dfdsddfd',
-          name: 'LOOM',
-          address: '0x0123f',
-          balance: 1,
-          date: '2018-11-14 18:23'
-        }
+        // {
+        //   nonce: '1',
+        //   name: 'ETH',
+        //   from: '0x0123f',
+        //   to: '0x04343',          
+        //   value: 10,
+        //   timestamp: "1541561978"
+        // }
       ]
     }
   },
@@ -46,6 +39,17 @@ export default {
     getTokenImg() {
       return require('@/assets/logo.png')
     }
+  },
+
+  mounted() {     
+    window.wallet.logs.txlistAll("0x3BDB8baC386815E07E0A089Dd8756FF32f678c63", "0x0", (result) => {
+      console.log("txlist", result)
+      this.transactions = result
+    })
+  },
+
+  methods: {
+
   }
 }
 </script>
@@ -54,9 +58,13 @@ export default {
   margin: 0 auto;    
   padding: 0 0 0 15px;
   width: 100%;
-  height: 180px;  
+  height: 130px;  
   background: #f8f8f8;
   color: #989898; 
+}
+.menu img {
+  width: 80px;
+  height: 80px;
 }
 .menu .desc {  
   font-size: 12px;
