33c11002e1e54f5c9aea3081f64f8faee5bf14ca
GunwooLee <hutswing5613@gmail.com>
2018-11-25 01:29:31 +0900

    show tokenlist

--- src/components/TokenList.vue
+++ src/components/TokenList.vue
@@ -6,35 +6,35 @@
           <img :src="getTokenImg" />
           <div class="detail">
             <p class="name">{{token.name}} : {{token.address}}</p>
-            <p class="desc">{{token.balance}}</p>        
-          </div>   
+            <p class="desc">{{token.balance}}</p>
+          </div>
           <div class="btns">
             <b-button>Withdraw</b-button>
             <b-button>History</b-button>
           </div>
-      </b-list-group-item>      
+      </b-list-group-item>
     </b-list-group>
   </div>
 </template>
 
 <script>
 
-export default {  
+export default {
   name: 'tokenlist',
 
   data() {
     return {
       tokens: [
-        {
-          name: 'ETH',
-          address: '0x0123f',
-          balance: 10
-        },
-        {
-          name: 'LOOM',
-          address: '0x0123f',
-          balance: 1
-        }
+        // {
+        //   name: 'ETH',
+        //   address: '0x0123f',
+        //   balance: 10
+        // },
+        // {
+        //   name: 'LOOM',
+        //   address: '0x0123f',
+        //   balance: 1
+        // }
       ]
     }
   },
@@ -43,19 +43,40 @@ export default {
     getTokenImg() {
       return require('@/assets/logo.png')
     }
-  }
+  },
+
+  mounted() {
+    for (let key of Object.keys(window.wallet.account.balances)){
+      this.tokens.unshift({name: window.wallet.option.erc20s[key][0].toUpperCase(),
+                        address: window.wallet.account.address(),
+                        balance: key === "0x0" ? window.web3.fromWei(window.wallet.account.balances[key]) : window.wallet.account.balances[key]})
+    }
+  },
+
+  methods: {
+
+  },
 }
 </script>
 <style scoped>
-.menu {    
-  margin: 0 auto;    
+.menu {
+  margin: 0 auto;
   padding: 0 0 0 15px;
-  max-width: 280px;
+  width: 100%;
+  height: 130px;
   background: #f8f8f8;
-  color: #989898; 
+  color: #989898;
 }
-.menu .desc {
-  max-width: 180px;
+.menu img {
+  width: 80px;
+  height: 80px;
+}
+.menu .name {
+  font-weight: bold;
   font-size: 12px;
 }
+.menu .desc {
+  font-size: 12px;
+}
+
 </style>
